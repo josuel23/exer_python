7c414d4ccbff34e9caa586e0669dbac2028b572b
josuel23 <josuel.silva@aluno.faculdadeimpacta.com.br>
2020-03-31 16:48:43 -0300

    Update multiplicação.py

--- ex_Python/multiplicação.py
+++ ex_Python/multiplicação.py
@@ -1,4 +1,5 @@
-n1  =  int(entrada('digite um valor'))
-n2  =  int(entrada('digite outro valor'))
+n1  =  int(input('digite um valor:'))
+n2  =  int(input('digite outro valor:'))
 mult = (n1  *  n2)
 print (mult)
+
